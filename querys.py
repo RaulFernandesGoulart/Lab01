444ad525fdfb1d2996218d696190513be9d7b7a6
Henrique Peixoto <107963741+Pxt77@users.noreply.github.com>
2025-02-23 22:47:41 -0300

    Update querys.py

--- querys.py
+++ querys.py
@@ -1,1 +1,35 @@
+querys = """
+query($first: Int!, $cursor: String) {
+  search(query: "stars:>1 sort:stars-desc", type: REPOSITORY, first: $first, after: $cursor) {
+    nodes {
+      ... on Repository {
+        name
+        owner { login }
+        stargazers { totalCount }
+        
+        pullRequests(states:[MERGED]) {
+          totalCount
+        }
+        
+        allIssues: issues(states:[OPEN,CLOSED]){
+          totalCount 
+        }
 
+        closedIssues: issues(states:[CLOSED]){
+          totalCount 
+        }
+
+        primaryLanguage{
+          name 
+        }
+
+        createdAt
+      }
+    }
+    pageInfo {
+      endCursor
+      hasNextPage
+    }
+  }
+}
+"""
